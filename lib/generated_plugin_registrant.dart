c259ce459f424ec3a648d6ea92b837af76dd253d
Tolik <sadullayevtolik@gmail.com>
2022-10-24 14:42:22 +0500

    commit

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,31 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:audio_service_web/audio_service_web.dart';
+import 'package:audio_session/audio_session_web.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  AudioServiceWeb.registerWith(registrar);
+  AudioSessionWeb.registerWith(registrar);
+  FlutterNativeTimezonePlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
